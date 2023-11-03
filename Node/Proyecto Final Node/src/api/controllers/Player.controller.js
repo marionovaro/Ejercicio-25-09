d3b9e2a7eb02634d62b9df8abebcc334562b2e67
marionovaro <marionovaro04@gmail.com>
2023-11-03 19:13:11 +0100

    proyecto final

---
+++ Node/Proyecto Final Node/src/api/controllers/Player.controller.js
@@ -0,0 +1,6 @@
+const { deleteImgCloudinary } = require("../../middleware/files.middleware");
+const enumOk = require("../../utils/enumOk");
+const Player = require("../models/Player.model");
+const Team = require("../models/Team.model");
+
+//! --------------- CREATE ----------------
---
+++ Node/Proyecto Final Node/src/api/models/Player.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+const PlayerSchema = new Schema(
+    {
+        name: {type: String, required: true},
+        position: {
+            type: String,
+            required: true,
+            enum: ["goalkeeper", "centre-back", "right-back", "left-back", "midfielder", "forward"]
+        },
+        number: {type: Number, required: true},
+        age: {type: Number, required: true},
+        marketvalue: {type: Number, required: true},
+        goals: {type: Number, required: true},
+        assists: {type: Number, required: true},
+        rating: {type: Number, required: true},
+        preferredfoot: {
+            type: String,
+            required: true,
+            enum: ["right", "left"]
+        },
+        image: {type: String, required: false},
+
+        trophies: {type: mongoose.Schema.Types.ObjectId, ref: "Team"} + {type: mongoose.Schema.Types.ObjectId, ref: "National"},
+        likes: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+        selected: [{type: mongoose.Schema.Types.ObjectId, ref: "User"}],
+    },
+    {
+        timestamps: true
+    }
+);
+const Player = mongoose.model("Player", PlayerSchema)
+module.exports = Player;
