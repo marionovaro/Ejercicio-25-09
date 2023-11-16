429f143fcf92396f7a54db67871c67c360ab59a6
marionovaro <marionovaro04@gmail.com>
2023-11-16 23:49:14 +0100

    ejercicios react

--- React/Apuntes Clase/Servicios/servicios/src/hooks/useErrorRegister.jsx
+++ React/Apuntes Clase/Servicios/servicios/src/hooks/useErrorRegister.jsx
@@ -1,3 +1,69 @@
 export const useErrorRegister = (res, setOk, setRes) => {
+    //? si la respuesta es ok ---- > directamente esta el status en la primera clave es decir: res.status
+    //? si la respuesta no esta ok--> res.response.status
+    //todo ------------------ 200 => TODO OK
+    if (res?.status == 200) {
+        setOk(() => true);
+        Swal.fire({
+        icon: "success",
+        title: "Welcome to my Page 💌",
+        showConfirmButton: false,
+        timer: 1500,
+        });
+        setRes({});
+    }
 
+    //todo ------------------- 409 => USER YA REGISTRADO
+
+    if (res?.response?.status === 409) {
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please , your email is incorrect !❎",
+        showConfirmButton: false,
+        timer: 1500,
+        });
+        setRes({});
+    }
+
+    //todo ------------------- CONTRASEÑA FORMATO INCORRECTO
+    if (res?.response?.data?.includes("validation failed: password")) { //? si la respuesta del error incluye esto (siempre es el mismo mensaje por lo que debería incluirla) es que el error es el de formato de contraseña
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Min 8 characters, 1 upper case, 1 lower case and a special character ❎",
+        showConfirmButton: false,
+        timer: 3000,
+        });
+        setRes({});
+    }
+
+      //todo ------------------- USERNAME EXISTE
+    if (
+        res?.response?.data?.includes(
+        "E11000 duplicate key error collection: userProyect.users" //? lo mismo que en el error anterior
+        )
+    ) {
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Sorry choose another name ❎",
+        showConfirmButton: false,
+        timer: 1500,
+        });
+        setRes({});
+    }
+
+      //todo -------------------- 500 => INTERNAL SERVER ERROR
+
+    if (res?.response?.status == 500) { //? siempre hay que gestionar esto por si falla
+        Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Interval server error!❎ Please try again.",
+        showConfirmButton: false,
+        timer: 1500,
+        });
+        setRes({});
+    }
 }
--- React/Apuntes Clase/Servicios/servicios/src/pages/Register/Register.jsx
+++ React/Apuntes Clase/Servicios/servicios/src/pages/Register/Register.jsx
@@ -27,7 +27,17 @@ const { handleSubmit, register } = useForm(); //? handleSubmit sirve para darle 
 //todo 3) FUNCION QUE GESTIONA LOS DATOS DEL FORMULARIO -------------------------------------------------
 
 const formSubmit = async (formData) => { //? esta función llama al servicio (API)
+console.log(formData) //? nos da esto: {name: 'hola', password: 'hola123', email: 'hola@gmail.com'}
 
+const customFormData = { //? le añadimos el genero pq es obligatorio y no lo tenemos en el html del return (apaño)
+  ...formData,
+  gender: "hombre"
+}
+//? hacemos llamada al servicio (API)
+setSend(true) //? estmos cargando porque es llamada asincrona
+setRes(await registerUser(customFormData)) //? seteamos la respuesta del serviio registerUser y le metemos el customFormData, como se setea la respuesta y esta cambia, se lanza el useEffect, que teneos res en al array de dependencias
+//? ------------------------------------------ al lanzarse el useEffect se lanza el useErrorRegister, que analiza los errores si hay.
+setSend(false) //? cuando ha acabado, se pone el send a false, pq ya no está cargando y vuelve a estar disponible el botón y cambia el texto que ambos tienen renderizado condicional
 }
 
 //todo 4) USEFFECT QUE GESTIONAN RESPUESTA Y LLAMAN AL CUSTOMHOOK
@@ -40,7 +50,77 @@ useEffect(() => { //? gestionamos los errores con el customhook (useErrorRegiste
 if (ok) { //? qué hacemos si el registro está ok: llevamos al usuario a la pagina de checkcode confirmation, renderizo otra pag.
 
 }
-  return (
-    <h1>Register</h1>
-  )
-}
+return (
+  <>
+    <div className="form-wrap">
+      <h1>Sign Up</h1>
+      <p>It’s free and only takes a minute.</p>
+      <form onSubmit={handleSubmit(formSubmit)}> {/* el formSubmit va a gestionar los datos enviados por el form */}
+        <div className="user_container form-group">
+          <input 
+            className="input_user"
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="false"
+            {...register("name", { required: true })} 
+          /> {/* estamos metiendo un input por cada campo requerido para crear el user y creamos una copia de user y se añade lo que va poniendo el user */}
+          <label htmlFor="custom-input" className="custom-placeholder">
+            username
+          </label>
+        </div>
+        <div className="password_container form-group">
+          <input
+            className="input_user"
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="false"
+            {...register("password", { required: true })}
+          />
+          <label htmlFor="custom-input" className="custom-placeholder">
+            password
+          </label>
+        </div>
+        <div className="email_container form-group">
+          <input
+            className="input_user"
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="false"
+            {...register("email", { required: true })}
+          />
+          <label htmlFor="custom-input" className="custom-placeholder">
+            email
+          </label>
+        </div>
+
+        <div className="btn_container">
+          {console.log(send)}
+          <button
+            className="btn"
+            type="submit"
+            disabled={send}
+            style={{ background: send ? "#49c1a388" : "#49c1a2" }}
+          >
+            {send ? "Cargando ...." : "Register"} {/* cuando se está enviando, por lo tanto que send es true, ponemos cargando o sin no register*/}
+          </button>
+        </div>
+        <p className="bottom-text">
+          <small>
+            By clicking the Sign Up button, you agree to our{" "}
+            <a href="#">Terms & Conditions</a> and{" "}
+            <a href="#">Privacy Policy</a>.
+          </small>
+        </p>
+      </form>
+    </div>
+    <footer>
+      <p>
+        Already have an account? <a href="#">Login Here</a>
+      </p>
+    </footer>
+  </>
+);
+};
